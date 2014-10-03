b89e4928cbb4baf3e6f128305ed1c9704b1b2fb9
Andrew Dunham <andrew@du.nham.ca>
2014-10-03 00:33:43 -0700

    Add syncthing Dockerfile

---
+++ syncthing/build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e              # Shell exits immediately on error
+set -o pipefail     # Exit if any stage in a pipeline fails
+set -x              # Log commands to STDERR
+
+######################################################################
+### INSTALL SYNCTHING
+
+export VERSION=v0.8.19
+export RELEASE=syncthing-linux-amd64-${VERSION}
+
+## Download syncthing
+curl -L -o /tmp/${RELEASE}.tar.gz \
+    https://github.com/calmh/syncthing/releases/download/${VERSION}/${RELEASE}.tar.gz
+
+## Extract and link syncthing
+tar zxf /tmp/${RELEASE}.tar.gz -C /usr/local
+ln -s /usr/local/${RELEASE}/syncthing /usr/local/bin/syncthing
+
+
+######################################################################
+### INSTALL INIT SCRIPT
+
+## Copy the init script
+cp /build/init /usr/local/bin/init
+chmod +x /usr/local/bin/init
+
+## Create the user we run as
+useradd -m syncthing
+
+## Create a dummy file where the default Sync directory would be - this
+## prevents sync from taking place until the repo path is updated manually.
+touch /home/syncthing/Sync
+
+
+######################################################################
+### CLEANUP
+
+## Remove temp files
+rm -rf /tmp/* /var/tmp/*
+
+## Finally, remove the entire /build directory (and ourselves)
+rm -rf /build
