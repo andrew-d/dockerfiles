f928db6ead7f73bf2900e512185ee5e7da195a9d
Andrew Dunham <andrew@du.nham.ca>
2014-10-11 02:28:13 -0700

    Compile sthttpd with more secure options

--- sthttpd-build/build.sh
+++ sthttpd-build/build.sh
@@ -15,8 +15,21 @@ curl -L -o sthttpd.tar.gz http://opensource.dyc.edu/pub/sthttpd/sthttpd-${STHTTP
 tar zxvf sthttpd.tar.gz
 cd sthttpd*
 
+## Disable various things that we don't want:
+### 1. Directory indexes
+sed -i -e 's/^#define GENERATE_INDEXES$/\/* #define GENERATE_INDEXES *\//' src/thttpd.h
+
+### 2. Server version
+sed -i -e 's/^#define SHOW_SERVER_VERSION$/\/* #define SHOW_SERVER_VERSION *\//' src/thttpd.h
+
+### 3. Verbose information in error pages
+sed -i -e 's/^#define EXPLICIT_ERROR_PAGES$/\/* #define EXPLICIT_ERROR_PAGES *\//' src/thttpd.h
+
+### 4. Names of index files (this removes e.g. index.cgi)
+sed -i -e 's/^#define INDEX_NAMES\s.*$/#define INDEX_NAMES "index.html", "index.htm"/' src/thttpd.h
+
 ## Build as static binary using musl
-## Note: the WEBDIR variable is described as:
+## Variables:
 ##      WEBDIR      The document root directory of your web page
 ./configure CC=/usr/local/musl/bin/musl-gcc CFLAGS="-static" WEBDIR="/data"
 make -j4
