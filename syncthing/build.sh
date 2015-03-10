b1cfa1c1afbd587375c1c9f096bc2e2d316aa151
Andrew Dunham <andrew@du.nham.ca>
2015-03-09 20:30:53 -0700

    syncthing: Use busybox base container, bump to 0.10.25

--- syncthing/build.sh
+++ syncthing/build.sh
@@ -1,21 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e              # Shell exits immediately on error
-set -o pipefail     # Exit if any stage in a pipeline fails
 set -x              # Log commands to STDERR
+
+######################################################################
+### INSTALL DEPENDENCIES
+
+opkg-install curl ca-certificates
+
+# So Go can find the certificates
+cat /etc/ssl/certs/*.crt > /etc/ssl/certs/ca-certificates.crt
 
 ######################################################################
 ### INSTALL SYNCTHING
 
-export VERSION=v0.10.5
+export VERSION=v0.10.25
 export RELEASE=syncthing-linux-amd64-${VERSION}
 
 ## Download syncthing
 curl -L -o /tmp/${RELEASE}.tar.gz \
-    https://github.com/calmh/syncthing/releases/download/${VERSION}/${RELEASE}.tar.gz
+    https://github.com/syncthing/syncthing/releases/download/${VERSION}/${RELEASE}.tar.gz
 
 ## Extract and link syncthing
-tar zxf /tmp/${RELEASE}.tar.gz -C /usr/local
+mkdir -p /usr/local/bin
+gunzip /tmp/${RELEASE}.tar.gz
+tar xf /tmp/${RELEASE}.tar -C /usr/local
 ln -s /usr/local/${RELEASE}/syncthing /usr/local/bin/syncthing
 
 
@@ -26,8 +35,16 @@ ln -s /usr/local/${RELEASE}/syncthing /usr/local/bin/syncthing
 cp /build/init /usr/local/bin/init
 chmod +x /usr/local/bin/init
 
+## Copy the run script
+cp /build/run-syncthing /usr/local/bin/run-syncthing
+chmod +x /usr/local/bin/run-syncthing
+
 ## Create the user we run as
-useradd -m syncthing
+adduser                 \
+    -D                  \
+    -h /home/syncthing  \
+    -s /bin/sh          \
+    syncthing
 
 ## Create a dummy file where the default Sync directory would be - this
 ## prevents sync from taking place until the repo path is updated manually.
